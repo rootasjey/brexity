a3c36f4f53ab9529deab3cbc888c0fb0be09013c
emmanuelEJd <44813764+emmanuelEJd@users.noreply.github.com>
2020-09-06 13:47:12 +0200

    add asset

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 
-import { Scene as PreloaderScene } from './scenes/preload';
+//import { Scene as PreloaderScene } from './scenes/preload';
 import { Scene as MainScene } from './scenes/main';
 
 const config = {
@@ -18,7 +18,7 @@ const config = {
     mode: Phaser.Scale.ENVELOP,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [PreloaderScene, MainScene],
+  scene: [/*PreloaderScene,*/ MainScene],
 };
 
 window.addEventListener('load', () => new Phaser.Game(config));
